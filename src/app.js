f1d92cefea939cd170b949329cdc74866b603d95
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-01-20 11:43:40 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["Mi perro", "El profesor", "Un pájaro", "Un alien"];
+  const action = ["comió", "rompió", "perdió", "se llevó"];
+  const what = ["mi tarea", "el proyecto", "mis apuntes", "mi libro"];
+  const when = ["ayer", "esta mañana", "hace un rato", "el lunes pasado"];
+
+  function generateExcuse() {
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  }
+
+  document
+    .getElementById("generateButton")
+    .addEventListener("click", function() {
+      document.querySelector(".excuse").textContent = generateExcuse();
+    });
+
+  document.querySelector(".excuse").textContent = generateExcuse();
 };
